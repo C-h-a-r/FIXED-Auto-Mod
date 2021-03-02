e69b9947bac526e3175821c31da79a735e3278b8
Charweee <68965006+charweeeee@users.noreply.github.com>
2021-03-02 18:32:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Fs = require('fs')
 
 client.on("guildMemberAdd", async (member) => {
   let UserJSON = JSON.parse(Fs.readFileSync("./DB/users.json"));
-UserJSON[member.id] = {
+  UserJSON[member.id] = {
                 warns: 0
             }
             Fs.writeFileSync("./DB/users.json", JSON.stringify(UserJSON));
@@ -17,11 +17,20 @@ UserJSON[member.id] = {
 
 let badWords = ["badword", "very bad word", "another bad word"]
 client.on("message", async message => {
+  let UserJSON = JSON.parse(Fs.readFileSync("./DB/users.json"));
+
+  if (!UserJSON[message.author.id]) {
+    if (message.author.bot) return;
+    UserJSON[message.author.id] = {
+      warns: 0
+    }
+    Fs.writeFileSync("./DB/users.json", JSON.stringify(UserJSON));
+  }
 for (i = 0; i < badWords.length; i++) {
 if (message.content.toLowerCase().includes(badWords[i])) {
 await message.delete();
 message.reply("no bad words");
-  let UserJSON = JSON.parse(Fs.readFileSync("./DB/users.json"));
+
 
 
 
@@ -31,8 +40,21 @@ message.reply("no bad words");
             UserJSON[message.author.id].warns += 1;
             Fs.writeFileSync("./DB/users.json", JSON.stringify(UserJSON));
 
+
+
+if (UserJSON[message.author.id].warns === 3) {
+ 
+(Fs.readFileSync("./DB/users.json"));
+  UserJSON[message.author.id].warns = 0;
+  Fs.writeFileSync("./DB/users.json", JSON.stringify(UserJSON));
+
+  const user = message.member
+  let role = message.guild.roles.cache.find(r => r.name === "Muted");
+
+  user.roles.add(role)
+  console.log(`lol i muted ${message.author}`)
+}
         }
-if (message.author.bot) return;
     }
 })
 
